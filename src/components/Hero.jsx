06ffe0ddb7becb1f641e51c8910588584652b1ea
Sambhav Jain <sambhavj343@gmail.com>
2026-06-04 22:01:04 +0530

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,234 @@
+import wave2 from "../assets/image copy.png";
+import wave from "../assets/image.png";
+import img2 from "../assets/img2.png";
+import img3 from "../assets/img3.png";
+import img4 from "../assets/img4.png";
+import img5 from "../assets/img5.png";
+import img6 from "../assets/img6.png";
+import img7 from "../assets/img7.png";
+import img8 from "../assets/img8.png";
+import img9 from "../assets/img9.png";
+import img10 from "../assets/img10.png";
+import circle from "../assets/image copy 2.png";
+import rectangular from "../assets/image copy 3.png";
+import rectangular2 from "../assets/image copy 4.png";
+import rectangular3 from "../assets/image copy 4.png";
+import rectangular4 from "../assets/image copy 4.png";
+import rectangular5 from "../assets/image copy 4.png";
+import rectangular6 from "../assets/image copy 4.png";
+import half from "../assets/image copy 5.png";
+import Line from "../assets/image copy 6.png";
+import Line2 from "../assets/image copy 6.png";
+import Line3 from "../assets/image copy 6.png";
+import Line4 from "../assets/image copy 6.png";
+import Line5 from "../assets/image copy 6.png";
+import curve from "../assets/image copy 7.png";
+import meetingImg from "../assets/image copy 8.png";
+import square from "../assets/image copy 9.png";
+import traingle from "../assets/image copy 10.png";
+import traingle2 from "../assets/image copy 10.png";
+import meetingImg2 from "../assets/image copy 11.png";
+import curve2 from "../assets/image copy 12.png";
+import box from "../assets/image copy 13.png";
+import circle2 from "../assets/image copy 14.png";
+import pfp from "../assets/image copy 15.png";
+import pfp2 from "../assets/image copy 16.png";
+import pfp3 from "../assets/image copy 17.png";
+import pfp4 from "../assets/image copy 18.png";
+import pfp5 from "../assets/image copy 19.png";
+import pfp6 from "../assets/image copy 20.png";
+import pfp7 from "../assets/image copy 21.png";
+import pfp8 from "../assets/image copy 22.png";
+function Hero() {
+  return (
+    <section className="hero">
+
+<h1 className="hero-title">
+  The thinkers and <br />
+
+<img src={Line} alt="shape" className="Line" />
+
+  <span className="highlight">
+    <img src={rectangular} alt="" className="rectangular" />
+    doers
+  </span>
+    
+  {" "}were{" "}
+
+  <span className="highlight">
+    <img src={rectangular2} alt="" className="rectangular2" />
+    changing
+  </span>
+
+  {" "}the status quo with
+  </h1><br /><br />
+
+<img src={half} alt="shape" className="half" />
+
+      <p>
+        We are a team of strategists, designers communicators, researchers. Togeather,
+we belive that progress only hghappens when you refuse to play things safe.
+      </p>
+      <img src={wave2} alt="" className="wave2" />
+      <img src={wave} alt="shape" className="wave" />
+      
+
+<div className="team-section">
+  <img src={img2} className="person p2" alt="" />
+  <img src={img3} className="person p3" alt="" />
+  <img src={img4} className="person p4" alt="" />
+  <img src={img5} className="person p5" alt="" />
+  <img src={img6} className="person p6" alt="" />
+  <img src={img7} className="person p7" alt="" />
+  <img src={img8} className="person p8" alt="" />
+  <img src={img9} className="person p9" alt="" />
+  <img src={img10} className="person p10" alt="" />
+
+</div>
+
+<img src={circle} alt="" className="circle" />
+<h1 className="hero-second">
+  Tomorraow should be <br />better {" "}than{" "} 
+  
+    <span className="highlight">
+    <img src={rectangular3} alt="" className="rectangular3" />
+  today</span>
+
+</h1>
+<img src={Line2} alt="shape" className="Line2" />
+
+<h2 className="hero-text">We are the terms of strategist, designers communactors, researchers. Togethor, we belive that progress only happens when you refuse to play things safe.</h2>
+<img src={meetingImg} alt="" className="team-photo" />
+<img src={square} alt="" className="Square" />
+<img src={curve} alt="" className="curve" />
+
+<div className="right-content">
+  <h1 className="hero-third">
+       <span className="highlight">
+    <img src={rectangular4} alt="" className="rectangular4" />
+    See
+    </span>
+     {" "}how{" "} we <br /> help your Progress
+  </h1><br /><br />
+
+  <h2 className="hero-graph">
+    We add a layer of fearless insights and action that allows change makers to accelerate their program in areas such as brand, design digital, comms and social research.
+  </h2>
+  <img src={meetingImg2} alt="" className="team-pic" />
+  <img src={Line3} alt="shape" className="Line3" />
+  <img src={traingle} alt="shape" className="traingle" />
+  <img src={traingle2} alt="shape" className="traingle2" />
+
+</div>
+
+<div className="Left-content">
+<h1 className="hero-four">What {" "}we{" "} 
+  <span className="highlight">
+    <img src={rectangular5} alt="" className="rectangular5" />
+  can </span> <br />offer you!</h1>
+    <img src={Line4} alt="shape" className="Line4" />
+</div>
+<img src={curve2} alt="shape" className="curve2" />
+
+
+<a href="#" className="footer-link"><div className="line">
+  <div className="footer-text">
+  <div className="left">Office of multiple<br /> intrest content</div>
+  <div className="center">Colaborative & Patnership</div>
+  <div className="right">➜</div>
+  </div>
+</div></a>
+
+<a href="#" className="footer-link">
+  <div className="line2">
+  <div className="footer-text">
+  <div className="left">The hanger US Air force<br /> digital experimental</div>
+  <div className="center">We talk about our weight</div>
+  <div className="right">➜</div>
+  </div>
+</div></a>
+
+<a href="#" className="footer-link"><div className="line3">
+  <div className="footer-text">
+  <div className="left">Delta faucet content,<br />social, digital</div>
+  <div className="center">Piloting digital confidence</div>
+  <div className="right">➜</div>
+  </div>
+</div></a>
+
+<img src={circle2} alt="shape" className="circle2" />
+<div className="line4"></div><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+<h1 className="hero-title">
+    <span className="highlight">
+    <img src={rectangular6} alt="" className="rectangular6" />
+  What</span> {" "}over{" "} customer<br /> say about us</h1>
+  
+      <img src={pfp} alt="" className="profile profile1" />
+      <img src={pfp2} alt="" className="profile profile2" />
+      <img src={pfp3} alt="" className="profile profile3" />
+      <img src={pfp4} alt="" className="profile profile4" />
+      <img src={pfp5} alt="" className="profile profile5" />
+      <img src={pfp6} alt="" className="profile profile6" />
+      <img src={pfp7} alt="" className="profile profile7" />
+      <img src={pfp8} alt="" className="profile profile8" />
+
+
+<img src={Line5} alt="shape" className="Line5" />
+<img src={box} alt="shape" className="box" /><br /><br /><br /><br /><br /><br />
+
+
+<div className="container">
+<footer className="footer">
+  <div className="footer-top">
+   <br /><br /><br /> <h2>
+      Subscribe to <br />
+      our newsletter
+    </h2>
+
+    <p>To make your stay special and even more memorable</p>
+
+    <button>Subscribe Now</button>
+  </div>
+
+  <hr />
+
+  <div className="footer-links">
+    <div>
+      <h3>Company</h3><br />
+      <a href="/">Home</a>
+      <a href="/">Studio</a>
+      <a href="/">Service</a>
+      <a href="/">Blog</a>
+    </div>
+
+    <div>
+      <h3>Terms & Policies</h3><br />
+      <a href="/">Privacy Policy</a>
+      <a href="/">Terms & Conditions</a>
+      <a href="/">Explore</a>
+      <a href="/">Accessibility</a>
+    </div>
+
+    <div>
+      <h3>Follow Us</h3><br />
+      <a href="/">Instagram</a>
+      <a href="/">LinkedIn</a>
+      <a href="/">Youtube</a>
+      <a href="/">Twitter</a>
+    </div>
+
+    <div>
+      <h3>Terms & Policies</h3><br />
+      <a href="/">1498w Fluton ste, STE 2D Chicgo, IL 63867</a>
+      <a href="/">(123) 456789000</a>
+      <a href="/">info@elementum.com</a>
+    </div>
+  </div>
+</footer></div>
+
+    </section>
+  );
+}
+
+export default Hero;
